ffda040d7b5ace52115ea079eddbc94db6f07590
Otto von Sperling <ottok92@gmail.com>
2019-05-27 00:27:50 -0300

    WIP refactoring the code; NEXT calculate column 'state' for each entry (e.g. failed 3 tests)

--- app.py
+++ app.py
@@ -21,12 +21,13 @@ app.css.append_css({
 
 orion_df = aux.load_testresults_todataframe('Data/')
 
-# Picked with http://tristen.ca/hcl-picker/#/hlc/6/1.05/251C2A/E98F55
-COLORS = ['#608F40', '#727D29', '#7C691D', '#80561E', '#7D4525']
-STATES = ['passed', 'failed_1', 'failed_2', 'failed_3', 'failed_all']
 
 
 # ---------------- DASHBOARD LAYOUT ------------------------ #
+
+# Picked with http://tristen.ca/hcl-picker/#/hlc/6/1.05/251C2A/E98F55
+COLORS = ['#608F40', '#727D29', '#7C691D', '#80561E', '#7D4525']
+STATES = ['passed', 'failed_1', 'failed_2', 'failed_3', 'failed_all']
 
 app.layout = html.Div(children=[
     html.H2(children='Orion Dashboard', style={
@@ -36,12 +37,12 @@ app.layout = html.Div(children=[
     html.Div([
         dcc.DatePickerRange(
             id='date-range-picker',
-            min_date_allowed = orion_df['created_at'].min().to_pydatetime(),
-            max_date_allowed = orion_df['created_at'].max().to_pydatetime(),
-            initial_visible_month = dt(orion_df['created_at'].max().to_pydatetime().year,
-                                       orion_df['created_at'].max().to_pydatetime().month, 1),
-            start_date = orion_df['created_at'].min().to_pydatetime(),
-            end_date = orion_df['created_at'].max().to_pydatetime(),
+            min_date_allowed = orion_df['created_at'].min().to_pydatetime().date(),
+            max_date_allowed = orion_df['created_at'].max().to_pydatetime().date(),
+            initial_visible_month = dt(orion_df['created_at'].max().to_pydatetime().date().year,
+                                       orion_df['created_at'].max().to_pydatetime().date().month, 1),
+            start_date = orion_df['created_at'].min().to_pydatetime().date(),
+            end_date = orion_df['created_at'].max().to_pydatetime().date(),
     )], className="row ", style={'marginTop': 30, 'marginBottom': 15}),
 
     # html.Div(id='output-container-date-picker-range-paid-search')
@@ -49,7 +50,7 @@ app.layout = html.Div(children=[
 
     dcc.Dropdown(
         id='categories',
-        options=[{'label': i, 'value': i} for i in orion_df['broader_category'].unique()],
+        options=[{'label': i, 'value': i} for i in STATES],
         multi=True
     ),
     dcc.Graph(
@@ -63,97 +64,97 @@ app.layout = html.Div(children=[
 
 # ---------------- DASHBOARD INTERACTIONS ------------------------ #
 
-@app.callback(
-    dash.dependencies.Output('speakers-in-daterange', 'figure'),
-    [
-        dash.dependencies.Input('categories', 'value'),
-        dash.dependencies.Input('date-range-picker', 'start_date'),
-        dash.dependencies.Input('date-range-picker', 'end_date'),
-    ])
+# @app.callback(
+#     dash.dependencies.Output('speakers-in-daterange', 'figure'),
+#     [
+#         dash.dependencies.Input('categories', 'value'),
+#         dash.dependencies.Input('date-range-picker', 'start_date'),
+#         dash.dependencies.Input('date-range-picker', 'end_date'),
+#     ])
 
-def update_scatterplot(categories, start_date, end_date):
-    if categories is None or categories == []:
-        categories = CATEGORIES
+# def update_scatterplot(categories, start_date, end_date):
+#     if categories is None or categories == []:
+#         categories = CATEGORIES
 
-    start_date = dt.strptime(start_date, '%Y-%m-%d %H:%M:%S')
-    end_date = dt.strptime(end_date, '%Y-%m-%d %H:%M:%S')
+#     start_date = dt.strptime(start_date, '%Y-%m-%d %H:%M:%S')
+#     end_date = dt.strptime(end_date, '%Y-%m-%d %H:%M:%S')
 
-    sub_df = orion_df_sub[(orion_df_sub['broader_category'].isin(categories))]
-    sub_df = sub_df.loc[[start_date <= x <= end_date for x in sub_df['created_at']]]
+#     sub_df = orion_df_sub[(orion_df_sub['broader_category'].isin(categories))]
+#     sub_df = sub_df.loc[[start_date <= x <= end_date for x in sub_df['created_at']]]
 
-    return {
-        'data': [
-            go.Scatter(
-                x=sub_df[(orion_df_sub.state == state)]['created_at'],
-                y=sub_df[(orion_df_sub.state == state)]['usd_pledged'],
-                text=sub_df[(orion_df_sub.state == state)]['name'],
-                mode='markers',
-                opacity=0.7,
-                marker={
-                    'size': 15,
-                    'color': color,
-                    'line': {'width': 0.5, 'color': 'white'}
-                },
-                name=state,
-            ) for (state, color) in zip(STATES, COLORS)
-        ],
-        'layout': go.Layout(
-            xaxis={'title': 'Date'},
-            yaxis={'title': 'USD pledged', 'type': 'log'},
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': 1},
-            hovermode='closest'
-        )
-    }
+#     return {
+#         'data': [
+#             go.Scatter(
+#                 x=sub_df[(orion_df_sub.state == state)]['created_at'],
+#                 y=sub_df[(orion_df_sub.state == state)]['usd_pledged'],
+#                 text=sub_df[(orion_df_sub.state == state)]['name'],
+#                 mode='markers',
+#                 opacity=0.7,
+#                 marker={
+#                     'size': 15,
+#                     'color': color,
+#                     'line': {'width': 0.5, 'color': 'white'}
+#                 },
+#                 name=state,
+#             ) for (state, color) in zip(STATES, COLORS)
+#         ],
+#         'layout': go.Layout(
+#             xaxis={'title': 'Date'},
+#             yaxis={'title': 'USD pledged', 'type': 'log'},
+#             margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+#             legend={'x': 0, 'y': 1},
+#             hovermode='closest'
+#         )
+#     }
 
 
-@app.callback(
-    dash.dependencies.Output('stats-in-daterange', 'figure'),
-    [
-        dash.dependencies.Input('categories', 'value'),
-        dash.dependencies.Input('speakers-in-daterange', 'relayoutData')
-    ])
-def update_bar_chart(categories, relayoutData):
-    if categories is None or categories == []:
-        categories = CATEGORIES
+# @app.callback(
+#     dash.dependencies.Output('stats-in-daterange', 'figure'),
+#     [
+#         dash.dependencies.Input('categories', 'value'),
+#         dash.dependencies.Input('speakers-in-daterange', 'relayoutData')
+#     ])
+# def update_bar_chart(categories, relayoutData):
+#     if categories is None or categories == []:
+#         categories = CATEGORIES
 
-    if (relayoutData is not None
-            and (not (relayoutData.get('xaxis.autorange') or relayoutData.get('yaxis.autorange')))):
-        x0 = dateutil.parser.parse(relayoutData['xaxis.range[0]'])
-        x1 = dateutil.parser.parse(relayoutData['xaxis.range[1]'])
-        y0 = 10 ** relayoutData['yaxis.range[0]']
-        y1 = 10 ** relayoutData['yaxis.range[1]']
+#     if (relayoutData is not None
+#             and (not (relayoutData.get('xaxis.autorange') or relayoutData.get('yaxis.autorange')))):
+#         x0 = dateutil.parser.parse(relayoutData['xaxis.range[0]'])
+#         x1 = dateutil.parser.parse(relayoutData['xaxis.range[1]'])
+#         y0 = 10 ** relayoutData['yaxis.range[0]']
+#         y1 = 10 ** relayoutData['yaxis.range[1]']
 
-        sub_df = orion_df[orion_df.created_at.between(x0, x1) & orion_df.usd_pledged.between(y0, y1)]
-    else:
-        sub_df = orion_df
+#         sub_df = orion_df[orion_df.created_at.between(x0, x1) & orion_df.usd_pledged.between(y0, y1)]
+#     else:
+#         sub_df = orion_df
 
-    stacked_barchart_df = (
-        sub_df[sub_df['broader_category'].isin(categories)]['state'].groupby(sub_df['broader_category'])
-        .value_counts(normalize=False)
-        .rename('count')
-        .to_frame()
-        .reset_index('state')
-        .pivot(columns='state')
-        .reset_index()
-    )
-    return {
-        'data': [
-            go.Bar(
-                x=stacked_barchart_df['broader_category'],
-                y=stacked_barchart_df['count'][state],
-                name=state,
-                marker={
-                    'color': color
-                }
-            ) for (state, color) in zip(STATES[::-1], COLORS[::-1])
-        ],
-        'layout': go.Layout(
-            yaxis={'title': 'Number of projects'},
-            barmode='stack',
-            hovermode='closest'
-        )
-    }
+#     stacked_barchart_df = (
+#         sub_df[sub_df['broader_category'].isin(categories)]['state'].groupby(sub_df['broader_category'])
+#         .value_counts(normalize=False)
+#         .rename('count')
+#         .to_frame()
+#         .reset_index('state')
+#         .pivot(columns='state')
+#         .reset_index()
+#     )
+#     return {
+#         'data': [
+#             go.Bar(
+#                 x=stacked_barchart_df['broader_category'],
+#                 y=stacked_barchart_df['count'][state],
+#                 name=state,
+#                 marker={
+#                     'color': color
+#                 }
+#             ) for (state, color) in zip(STATES[::-1], COLORS[::-1])
+#         ],
+#         'layout': go.Layout(
+#             yaxis={'title': 'Number of projects'},
+#             barmode='stack',
+#             hovermode='closest'
+#         )
+#     }
 
 
 # ---------------- MAIN ------------------------ #
