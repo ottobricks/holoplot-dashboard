01fd05bf0b101629a9381f087215a1df6e01c1be
Otto von Sperling <ottok92@gmail.com>
2019-05-28 11:24:20 -0300

    WORKING:     + date picker     + type of visualization     + dropdown of features

--- app.py
+++ app.py
@@ -27,8 +27,7 @@ orion_df = aux.load_testresults_todataframe('Data/')
 
 # Picked with http://tristen.ca/hcl-picker/#/hlc/6/1.05/251C2A/E98F55
 COLORS = ['#608F40', '#727D29', '#7C691D', '#80561E', '#7D4525']
-f_states = ['passed', 'failed_1', 'failed_2', 'failed_3', 'failed_all']
-t_states = ['response', 'polarity', 'rub+buzz', 'thd']
+
 
 app.layout = html.Div(children=[
     html.H2(children='Orion Dashboard', style={
@@ -42,24 +41,24 @@ app.layout = html.Div(children=[
             max_date_allowed = orion_df['created_at'].max().to_pydatetime().date(),
             initial_visible_month = dt(orion_df['created_at'].max().to_pydatetime().date().year,
                                        orion_df['created_at'].max().to_pydatetime().date().month, 1),
-            start_date = orion_df['created_at'].min().to_pydatetime().date(),
+            start_date = orion_df['created_at'].max().to_pydatetime().date() - timedelta(days=5),
             end_date = orion_df['created_at'].max().to_pydatetime().date(),
-    )], className="row ", style={'marginTop': 30, 'marginBottom': 15}),
-
-    # html.Div(id='output-container-date-picker-range-paid-search')
-    # ], className="row ", style={'marginTop': 30, 'marginBottom': 15}),
+            display_format='D MMM, YYYY',
+    )], className="row", style={'marginTop': 30, 'marginBottom': 15}),
     
     dcc.RadioItems(
-        id='state_modes',
+        id='radio_states',
         options=[
-            {'label': 'Failure Rate', 'value': 'f_states'},
-            {'label': 'Test Points', 'value': 't_states'}
+            {'label': 'Failure Rate', 'value': 'f_rate'},
+            {'label': 'Test Points', 'value': 't_points'},
+            {'label': 'Devices', 'value': 'd_specs'}
         ],
-        value='f_states'
+        value='f_rate',
+        labelStyle={'display': 'inline-block'}
     ),
     
     dcc.Dropdown(
-        id='states',
+        id='dropdown_states',
         #options=[{'label': i, 'value': i} for i in STATES],
         multi=True
     ),
@@ -75,48 +74,66 @@ app.layout = html.Div(children=[
 # ---------------- DASHBOARD INTERACTIONS ------------------------ #
 
 @app.callback(
-    dash.dependencies.Output('speakers-in-daterange', 'figure'),
+    dash.dependencies.Output('dropdown_states', 'options'),
     [
-        dash.dependencies.Input('states', 'value'),
+        dash.dependencies.Input('radio_states', 'value'),
         dash.dependencies.Input('date-range-picker', 'start_date'),
-        dash.dependencies.Input('date-range-picker', 'end_date'),
-    ])
+        dash.dependencies.Input('date-range-picker', 'end_date')
+    ]
+)
+def update_dropdown_states(mode, start, end):
+    '''
+    '''
+    if mode == 'f_rate':
+        return [{'label': ' '.join(i.split('_')).title(), 'value': i} for i in ['passed', 'failed_1', 'failed_2', 'failed_3', 'failed_all']]
+    
+    elif mode == 't_points':
+        return [{'label': i.title(), 'value': i} for i in ['response', 'polarity', 'rub+buzz', 'thd']]
 
-def update_scatterplot(states, start_date, end_date):
-    if states is None or states == []:
-        states = f_states
+    elif mode == 'd_specs':
+        if start is not None and end is not None:
+            return [{'label': i.title(), 'value': i} for i in orion_df.id.loc[orion_df.created_at.between(dt.strptime(start, '%Y-%m-%d'), dt.strptime(end, '%Y-%m-%d'))]]
 
-    start_date = dt.strptime(start_date, '%Y-%m-%d')
-    end_date = dt.strptime(end_date, '%Y-%m-%d')
+# @app.callback(
+#     dash.dependencies.Output('speakers-in-daterange', 'figure'),
+#     [
+#         dash.dependencies.Input('date-range-picker', 'start_date'),
+#         dash.dependencies.Input('date-range-picker', 'end_date'),
+#         dash.dependencies.Input('dropdown_states', 'value')
+#     ]
+# )
+# def update_scatterplot(start_date, end_date, states):
+#     start_date = dt.strptime(start_date, '%Y-%m-%d')
+#     end_date = dt.strptime(end_date, '%Y-%m-%d')
 
-    sub_df = orion_df[(orion_df.state.isin(states))]
-    sub_df = sub_df.loc[[start_date <= x <= end_date for x in sub_df.created_at.date()]]
+#     sub_df = orion_df[(orion_df.state.isin(states))]
+#     sub_df = sub_df.loc[[start_date <= x <= end_date for x in sub_df.created_at.date()]]
 
-    return {
-        'data': [
-            go.Scatter(
-                x = sub_df[(orion_df_sub.state == state)].created_at.date(),
-                y = sub_df[(orion_df_sub.state == state)].created_at.time(),
-                text = sub_df[(orion_df_sub.state == state)].id,
+#     return {
+#         'data': [
+#             go.Scatter(
+#                 x = sub_df[(orion_df_sub.state == state)].created_at.date(),
+#                 y = sub_df[(orion_df_sub.state == state)].created_at.time(),
+#                 text = sub_df[(orion_df_sub.state == state)].id,
                 
-                mode='markers',
-                opacity=0.7,
-                marker={
-                    'size': 15,
-                    'color': color,
-                    'line': {'width': 0.5, 'color': 'white'}
-                },
-                name=state,
-            ) for (state, color) in zip(states, COLORS)
-        ],
-        'layout': go.Layout(
-            xaxis={'title': 'Date'},
-            yaxis={'title': 'Time'},
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': 1},
-            hovermode='closest'
-        )
-    }
+#                 mode='markers',
+#                 opacity=0.7,
+#                 marker={
+#                     'size': 15,
+#                     'color': color,
+#                     'line': {'width': 0.5, 'color': 'white'}
+#                 },
+#                 name=state,
+#             ) for (state, color) in zip(states, COLORS)
+#         ],
+#         'layout': go.Layout(
+#             xaxis={'title': 'Date'},
+#             yaxis={'title': 'Time'},
+#             margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+#             legend={'x': 0, 'y': 1},
+#             hovermode='closest'
+#         )
+#     }
 
 
 # @app.callback(
@@ -172,5 +189,5 @@ def update_scatterplot(states, start_date, end_date):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    debug = os.environ.get('PRODUCTION') is None
-    app.run_server(debug=debug, host='0.0.0.0', port=port)
+    #debug = os.environ.get('PRODUCTION') is None
+    app.run_server(debug=True, host='0.0.0.0', port=port)
